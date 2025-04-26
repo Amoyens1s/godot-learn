44264a339efc41d1b031799f852ada6cf374673a
amoyensis <amoyensis@outlook.com>
2025-04-27 00:17:42 +0800

    feat:猪突猛进

---
+++ 03.勇者传说/scripts/enemy.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+class_name Enemy
+
+enum Direction {
+	LEFT = -1,
+	RIGHT = +1,
+}
+
+@onready var graphics: Node2D = $Graphics
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var state_machine: StateMachine = $StateMachine
+
+@export var direction := Direction.LEFT:
+	set(v):
+		direction = v
+		if not is_node_ready():
+			await ready
+		graphics.scale.x = -direction
+
+@export var max_speed: float = 180
+@export var accleleration: float = 2000
+
+var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+
+
+func move(speed: float, delta: float) -> void:
+	velocity.x = move_toward(velocity.x, speed * direction, accleleration * delta)
+	velocity.y += default_gravity * delta
+	
+	move_and_slide()
