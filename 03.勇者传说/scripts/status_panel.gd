84e9073ac0ef22b6e0edb1a9de3490380c3074b3
amoyensis <amoyensis@outlook.com>
2025-04-27 13:27:46 +0800

    feat: 场景转换血条不会重复显示动画

--- 03.勇者传说/scripts/status_panel.gd
+++ 03.勇者传说/scripts/status_panel.gd
@@ -12,19 +12,22 @@ func _ready() -> void:
 		stats = Game.player_stats
 
 	stats.health_changed.connect(update_health)
-	update_health()
+	update_health(true)
 
 	stats.energy_changed.connect(update_energy)
-	update_energy()
+	update_energy(true)
 
 
-func update_health() -> void:
+func update_health(skip_anim := false) -> void:
 	var percentage := stats.health / float(stats.max_health)
 	health_bar.value = percentage
-	create_tween().tween_property(eased_health_bar, "value", percentage, 0.3)
+
+	if skip_anim:
+		eased_health_bar.value = percentage
+	else:
+		create_tween().tween_property(eased_health_bar, "value", percentage, 0.3)
 
 
-func update_energy() -> void:
+func update_energy(skip_anim := false) -> void:
 	var percentage := stats.energy / stats.max_energy
 	energy_bar.value = percentage
-	create_tween().tween_property(energy_bar, "value", percentage, 0.3)
