2f2f2a076935a13846452483297ee00933502679
amoyensis <amoyensis@outlook.com>
2025-04-29 13:53:46 +0800

    feat: 空格ciallo游戏

--- 04.仿谷歌恐龙跑酷/scenes/main.gd
+++ 04.仿谷歌恐龙跑酷/scenes/main.gd
@@ -3,27 +3,42 @@ extends Node
 const DINO_START_POS := Vector2i(150, 485)
 const CAM_START_POS := Vector2i(576, 324)
 
+var score: int
+const SCORE_MODIFIER: int = 10
 var speed: float
 const START_SPEED: float = 10.0
 const MAX_SPEED: int = 25
 var screen_size: Vector2i
+var game_running: bool
 
 func _ready() -> void:
 	screen_size = get_window().size
 	new_game()
 
 func new_game():
+	score = 0
+	game_running = false
+
 	$Dino.position = DINO_START_POS
 	$Dino.velocity = Vector2i(0, 0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0, 0)
 
 func _physics_process(delta: float) -> void:
-	speed = START_SPEED
-	
-	$Dino.position.x += speed
-	$Camera2D.position.x += speed
+	if game_running:
+		speed = START_SPEED
+		
+		$Dino.position.x += speed
+		$Camera2D.position.x += speed
+		score += speed
+		show_score()
+		# update ground position
+		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
 
-	# update ground position
-	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-		$Ground.position.x += screen_size.x
+func show_score():
+	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
